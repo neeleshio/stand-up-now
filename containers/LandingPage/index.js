d7bdae902311eaba257eea1d213133ef50c4f15d
neeleshio <neelesh@campk12.com>
2023-05-20 22:04:51 +0530

    clear localstorage

--- containers/LandingPage/index.js
+++ containers/LandingPage/index.js
@@ -1,12 +1,15 @@
-import ControlCard from '@/components/SubControlCard';
 import Header from '@/components/Header';
 import Navbar from '@/components/Navbar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import ControlCenter from '../ControlCenter';
 import { StyledLandingPage } from './styles';
 import SubController from '../SubController';
 
 function LandingPage() {
+    useEffect(() => {
+        localStorage.clear();
+    }, []);
+
     return (
         <StyledLandingPage>
             <Navbar />
